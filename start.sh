5029e968d2064eb3faa8a3840d562968397cda2f
hjdtpz <hjdtpz@outlook.com>
2023-11-19 18:29:37 +0000

    public link

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ./VC_inference.py --model_dir ../VoiceModels/G_latest.pth --config_dir ../VoiceModels/config.json
+python ./VC_inference.py --model_dir ../VoiceModels/G_latest.pth --config_dir ../VoiceModels/config.json --share True
